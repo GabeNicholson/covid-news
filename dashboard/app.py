1b7c905810fa465efbaae2fdd01fce2b9f5a417f
Gabenicholson <gabriel.nicholson@dal.ca>
2022-09-12 18:03:16 -0500

    update dash app

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,9 +1,17 @@
 import dash_bootstrap_components as dbc
-import dash
+from dash import Dash, dcc
+import pandas as pd
 
-app = dash.Dash(__name__, use_pages=False, external_stylesheets=[dbc.themes.LUX])
+app = Dash(__name__, use_pages=False, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-
+app.layout = dbc.Container([
+	dcc.Markdown('Covid-19 Sentiment Analysis', style={'textAlign': 'center'}, className='mb-3')
+	dbc.Row([
+		dbc.Col([
+			
+		])
+	])
+])
 
 if __name__ == '__main__':
 	app.run_server(debug=True, port=6000)
